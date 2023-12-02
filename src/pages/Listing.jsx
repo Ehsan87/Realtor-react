8bbdaacbcae6ee7ba21c08a2893c65b1034a2b6d
Ehsan87 <ehsan.samani6@live.com>
2023-12-02 00:45:29 +0000

    listing page and use Swiper to add image slider

---
+++ src/pages/Listing.jsx
@@ -0,0 +1,56 @@
+import { doc, getDoc } from "firebase/firestore";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import { db } from "../firebase";
+import Spinner from "../Components/Spinner";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Autoplay, Navigation, Pagination } from "swiper/modules";
+import "swiper/css/bundle";
+
+function Listing() {
+  const params = useParams();
+  const [listing, setListing] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchListing() {
+      const docRef = doc(db, "listings", params.listingId);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setListing(docSnap.data());
+        setLoading(false);
+      }
+    }
+    fetchListing();
+  }, [params.listingId]);
+  if (loading) {
+    return <Spinner />;
+  }
+
+  return (
+    <main>
+      <Swiper
+        modules={[Autoplay, Navigation, Pagination, EffectFade]}
+        slidesPerView={1}
+        navigation
+        pagination={{ type: "progressbar" }}
+        effect="fade"
+        autoplay={{ delay: 3000 }}
+      >
+        {listing.imgUrls.map((url, index) => (
+          <SwiperSlide key={index}>
+            <div
+              className="relative w-full overflow-hidden h-[300px]"
+              style={{
+                background: `url(${listing.imgUrls[index]}) center no-repeat`,
+                backgroundSize: "cover",
+              }}
+            ></div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </main>
+  );
+}
+
+export default Listing;
