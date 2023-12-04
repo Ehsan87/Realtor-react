cd5fafde0776e2280868c0b092c0c52f36c52601
Ehsan87 <ehsan.samani6@live.com>
2023-12-04 00:29:14 +0000

    fixed leaflet popup

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -157,9 +157,7 @@ function Listing() {
             <Marker
               position={[listing.geolocation.lat, listing.geolocation.lng]}
             >
-              <Popup>
-                A pretty CSS3 popup. <br /> Easily customizable.
-              </Popup>
+              <Popup>{listing.address}</Popup>
             </Marker>
           </MapContainer>
         </div>
