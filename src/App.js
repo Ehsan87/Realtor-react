29ff45362bc5013c9db7e4fcbcf4626b9d0db9d6
Ehsan87 <ehsan.samani6@live.com>
2023-12-05 00:14:52 +0000

    create places for rent and sale pages

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import PrivateRoute from "./Components/PrivateRoute";
 import CreateListing from "./pages/CreateListing";
 import EditListing from "./pages/EditListing";
 import Listing from "./pages/Listing";
+import Category from "./pages/Category";
 
 function App() {
   return (
@@ -27,6 +28,8 @@ function App() {
           <Route path="/sign-in" element={<SignIn />} />
           <Route path="/sign-up" element={<SignUp />} />
           <Route path="/offers" element={<Offers />} />
+          <Route path="/category/:categoryName" element={<Category />} />
+
           <Route
             path="/category/:categoryName/:listingId"
             element={<Listing />}
